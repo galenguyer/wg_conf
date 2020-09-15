3843a11de018eec90340bff3283c53bb34a3b339
Galen Guyer <galen@galenguyer.com>
2020-09-15 00:28:56 -0400

    Same logic was also broken in del_peer, ffs

--- wg_conf/__init__.py
+++ wg_conf/__init__.py
@@ -166,22 +166,23 @@ class WireguardConfig:
 
 
     def del_peer(self, publickey):
-        start_index = 1
+        index = 0
+        start_index = 0
         end_index = 0
         section_started = False
         correct_section = False
         for line in self._lines:
             if line.strip() == '[Peer]':
                 section_started = True
-                start_index = start_index + 1
+                start_index = index
             if section_started and not correct_section:
                 pline = self.parse_line(line)
                 if pline[0].lower() == 'publickey' and pline[1] == publickey:
                     correct_section = True
-                start_index = start_index + 1
             if correct_section and line.strip() in ['', '[Peer]']:
                 break
             end_index = end_index + 1
+            index = index + 1
         for _ in range(start_index, end_index):
             self._lines.pop(start_index)
         self.parse_lines()
